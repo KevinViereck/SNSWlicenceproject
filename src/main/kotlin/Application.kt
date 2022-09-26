ee6f4e1fa3b1842ca60ca8db60b3d6bb4dd7d4f3
benjmainroper <ben.roper2@studytafensw.edu.au>
2022-09-27 09:51:41 +1000

    LogBookRoute update routes

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -8,10 +8,11 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.*
 import org.litote.kmongo.*
+import routes.LogBookRoute
 
 val client = KMongo.createClient()
-val database = client.getDatabase("DATABASE_NAME")
-val collectionName = database.getCollection<DATA_TYPE>("COLLECTION_NAME")
+val database = client.getDatabase("Logbook")
+
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
@@ -29,55 +30,4 @@ fun Application.start() {
     install(ContentNegotiation) {
         json()
     }
-
-    routing {
-        route("/contentName") {
-            get{
-                val data = collectionName.find().toList()
-                call.respond(data)
-            }
-            get("/{id}"){
-                val id = call.parameters["id"].toString()
-                val filter = "{_id:ObjectId('$id')}"
-                val entity = collectionName.findOne(filter)
-                if (entity != null) {
-                    call.respond(entity)
-                } else {
-                    call.respond((HttpStatusCode.NotFound))
-                }
-            }
-            post{
-                val entity = call.receive<DATA_TYPE>()
-                collectionName.insertOne(entity)
-                call.respond(HttpStatusCode.Created, entity)
-            }
-            put{
-                val entity = call.receive<DATA_TYPE>()
-                val result = collectionName.updateOne(entity)
-                if (result.modifiedCount.toInt() == 1){
-                    call.respond(HttpStatusCode.OK, entity)
-                } else {
-                    call.respond(HttpStatusCode.NotFound)
-                }
-            }
-            delete("/{id}"){
-                val id = call.parameters["id"].toString()
-                val filter = "{_id:ObjectId('$id')}"
-                val result = collectionName.deleteOne(filter)
-                if (result.deletedCount.toInt() == 1) {
-                    call.respond(HttpStatusCode.NoContent)
-                } else {
-                    call.respond(HttpStatusCode.NotFound)
-                }
-            }
-        }
-    }
 }
-
-@Serializable
-data class DATA_TYPE(
-    val firstProperty: String, // can use any type
-    val secondProperty: String,
-    val thirdProperty: Int,
-    @Serializable(with = ObjectIdAsStringSerializer::class) val _id: Id<DATA_TYPE> = newId(),
-)
---
+++ src/main/kotlin/routes/LogBookRoute.kt
@@ -0,0 +1,58 @@
+package routes
+
+import com.mongodb.client.MongoDatabase
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import model.LogBook
+import org.litote.kmongo.*
+
+
+fun Route.logBookRoute (db: MongoDatabase) {
+    val logbook = db.getCollection<LogBook>("logbook")
+
+    route("/logbook") {
+        get{
+            val data = logbook.find().toList()
+            call.respond(data)
+        }
+        get("/{id}"){
+            val id = call.parameters["id"].toString()
+            val filter = "{_id:ObjectId('$id')}"
+            val entity = logbook.findOne(filter)
+            if (entity != null) {
+                call.respond(entity)
+            } else {
+                call.respond((HttpStatusCode.NotFound))
+            }
+        }
+        post{
+            val entity = call.receive<LogBook>()
+            logbook.insertOne(entity)
+            call.respond(HttpStatusCode.Created, entity)
+        }
+        put{
+            val entity = call.receive<LogBook>()
+            val result = logbook.updateOne(entity)
+            if (result.modifiedCount.toInt() == 1){
+                call.respond(HttpStatusCode.OK, entity)
+            } else {
+                call.respond(HttpStatusCode.NotFound)
+            }
+        }
+        delete("/{id}"){
+            val id = call.parameters["id"].toString()
+            val filter = "{_id:ObjectId('$id')}"
+            val result = logbook.deleteOne(filter)
+            if (result.deletedCount.toInt() == 1) {
+                call.respond(HttpStatusCode.NoContent)
+            } else {
+                call.respond(HttpStatusCode.NotFound)
+            }
+        }
+    }
+}
+
+
