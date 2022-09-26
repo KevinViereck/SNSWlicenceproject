a0ee1b97fd660d69156599733cf9b82ee7b209db
benjmainroper <ben.roper2@studytafensw.edu.au>
2022-09-27 09:34:31 +1000

    Updated User Class

--- src/main/kotlin/model/User.kt
+++ src/main/kotlin/model/User.kt
@@ -1,10 +1,16 @@
 package model
+import ObjectIdAsStringSerializer
+import kotlinx.serialization.Serializable
+import org.litote.kmongo.Id
+import org.litote.kmongo.newId
 
+@Serializable
 data class User (
     val firstName: String,
     val lastName: String,
     val email: String,
     val mobile: Int,
-    val password:,
-
+    val password:String,
+    @Serializable(with = ObjectIdAsStringSerializer::class)
+    val _id: Id<User> = newId()
         )
